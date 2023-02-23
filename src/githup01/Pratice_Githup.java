364b3780ff0fca6ad8733288043391e43834cc95
mrtgklp <109852097+mrtgklp@users.noreply.github.com>
2023-02-24 00:05:35 +0300

    v6

--- src/githup01/Pratice_Githup.java
+++ src/githup01/Pratice_Githup.java
@@ -4,5 +4,6 @@ public class Pratice_Githup {
     public static void main(String[] args) {
         System.out.println("1. Calısmam");
         System.out.println("v3");
+        System.out.println("bugun hava yagıslı");
     }
 }
